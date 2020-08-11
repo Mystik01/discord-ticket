ccd8f08f0e6e138d6018e84905b42e25652ca71d
Mystik <65687597+Mystik01@users.noreply.github.com>
2020-08-11 18:55:02 +0100

    Delete database.js

--- Ticket Bot/database.js
+++
@@ -1,6 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASS, {
-    dialect: 'mysql',
-    host: process.env.DB_HOST
-});
