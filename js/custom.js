8d55637634ec803dd64d4157546a72920d6c10f7
Mystik <65687597+Mystik01@users.noreply.github.com>
2020-08-12 21:25:56 +0100

    website

---
+++ js/custom.js
@@ -0,0 +1,81 @@
+/**************************************
+    Template Name: Micrology
+    Created By: HTML.Design
+    https://html.design
+**************************************/
+
+(function($) {
+    "use strict";
+
+/**************************************
+TOOLTIP
+**************************************/
+
+    $(function() {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
+
+/**************************************
+CLICK SCROLLING
+**************************************/
+
+    $('a[href*="#"]')
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+          && 
+          location.hostname == this.hostname
+        ) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000, function() {
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
+          }
+        }
+    });
+
+/**************************************
+BACK TO TOP
+**************************************/
+    jQuery(window).scroll(function(){
+        if (jQuery(this).scrollTop() > 1) {
+            jQuery('.dmtop').css({bottom:"25px"});
+        } else {
+            jQuery('.dmtop').css({bottom:"-100px"});
+        }
+    });
+    jQuery('.dmtop').click(function(){
+        jQuery('html, body').animate({scrollTop: '0px'}, 800);
+        return false;
+    });
+
+/**************************************
+SLIDE WIDGETS
+**************************************/
+
+    $(document).ready(function() {
+        $('#nav-expander').on('click', function(e) {
+            e.preventDefault();
+            $('body').toggleClass('nav-expanded');
+        });
+        $('#nav-close').on('click', function(e) {
+            e.preventDefault();
+            $('body').removeClass('nav-expanded');
+        });
+    });
+    
+})(jQuery);
